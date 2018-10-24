ca2ba8780ffc061bd5d7cd62b6a43d0900ad50d3
Zhitao He <zhitao.he@ri.se>
2018-10-24 22:01:32 +0200

    enlarge memory sizes to accomodate up to 32 nodes

--- examples/6tisch/sensor-node/node.h
+++ examples/6tisch/sensor-node/node.h
@@ -6,15 +6,15 @@
 #define MAX_PKTS 1000
 #define DEFAULT_PAYLOAD_LEN 50
 #define MAX_RUNS 1
-#define MAX_SOURCE_NODES 16
+#define MAX_SOURCE_NODES 32
 #define NSEQNOS 4
 
 /* random interval uniformly distributed btw. min and max values,
  * in terms of etimer clock ticks
  * to set a fixed interval, simply set min and max to the same value
  */
-#define SEND_INTERVAL_MIN (CLOCK_SECOND * 180 / 1000)
-#define SEND_INTERVAL_MAX (CLOCK_SECOND * 180 / 1000)
+#define SEND_INTERVAL_MIN (CLOCK_SECOND * 360 / 1000)
+#define SEND_INTERVAL_MAX (CLOCK_SECOND * 360 / 1000)
 #define SEND_INTERVAL_DELTA (SEND_INTERVAL_MAX - SEND_INTERVAL_MIN)
 #define SEND_INTERVAL_AVG ((SEND_INTERVAL_MAX + SEND_INTERVAL_MIN) / 2)
 #define SEND_INTERVAL (SEND_INTERVAL_MIN + random_rand() % (SEND_INTERVAL_DELTA + 1))
--- examples/6tisch/sensor-node/project-conf.h
+++ examples/6tisch/sensor-node/project-conf.h
@@ -69,7 +69,7 @@
 #define ORCHESTRA_CONF_COLLISION_FREE_HASH 1
 #define ORCHESTRA_CONF_EBSF_PERIOD 397
 #define ORCHESTRA_CONF_COMMON_SHARED_PERIOD 31
-#define ORCHESTRA_CONF_UNICAST_PERIOD 16
+#define ORCHESTRA_CONF_UNICAST_PERIOD 32
 
 /* TSCH customizations */
 /* #define TSCH_CONF_DEFAULT_TIMESLOT_LENGTH 8000 // 8 ms *\/ */
@@ -78,6 +78,7 @@
 /* #define TSCH_CONF_DEQUEUED_ARRAY_SIZE 8 // must >= QUEUEBUF_CONF_NUM */
 /* #define TSCH_CONF_MAX_INCOMING_PACKETS 4 // tsch default 4 */
 #define TSCH_QUEUE_CONF_NUM_PER_NEIGHBOR 8 // default QUEUEBUF_CONF_NUM
+#define TSCH_SCHEDULE_CONF_MAX_LINKS 64
 
 /*******************************************************/
 /************* Other system configuration **************/
